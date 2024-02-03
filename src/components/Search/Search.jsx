1ddc662a5fa7c606359a863095c41650b269b4bc
Vladislav <98421574+Sicilianii@users.noreply.github.com>
2024-02-03 19:38:53 +0200

    day 2
    add "parts"

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,11 @@
 import './Search.css';
+import SearchForm from "./parts/SearchForm/SearchForm";
 
 export default function Search() {
     return(
-        <></>
+        <div className={'flex justify-between'}>
+            <span>CommuniHub</span>
+            <SearchForm />
+        </div>
     );
 }
