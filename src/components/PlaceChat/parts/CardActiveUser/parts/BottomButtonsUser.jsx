1e00a906da6596f15e04af18b8722975236e61b4
Vladislav <98421574+Sicilianii@users.noreply.github.com>
2024-02-04 16:00:25 +0200

    add card active users

---
+++ src/components/PlaceChat/parts/CardActiveUser/parts/BottomButtonsUser.jsx
@@ -0,0 +1,11 @@
+import './BottomButtonsUser.css';
+
+export default function BottomButtonsUser() {
+    return(
+        <div>
+            <button></button>
+            <button></button>
+            <button></button>
+        </div>
+    )
+}
