910088fd36f86edb7dd4aa7a6e9bba4896387bc8
Vladislav <98421574+Sicilianii@users.noreply.github.com>
2024-02-11 18:55:32 +0200

    add list chats

--- src/components/YourChats/ChatList/ChatList.jsx
+++ src/components/YourChats/ChatList/ChatList.jsx
@@ -2,8 +2,24 @@ import './ChatList.css';
 
 export default function ChatList() {
     return(
-        <ul className={'list-chats'}>
-            <li className={'list-chats__item'}>Group Chat</li>
+        <ul className={'list-group'}>
+            <li className={'list-group__item'}>
+                <button className={'list-group__btn'}>
+                    <span className={'list-group__item-name'}>Group Chat</span>
+                    <span className={'list-group__item-arrow'}>v</span>
+                </button>
+                <ul className={'list-chats'}>
+                    <li className={'list-chats__item'}>Mike
+                        <div className={'list-chats__count-mess'}>2</div>
+                    </li>
+                    <li className={'list-chats__item'}>Luice
+                        <div className={'list-chats__count-mess'}>2</div>
+                    </li>
+                    <li className={'list-chats__item'}>German
+                        <div className={'list-chats__count-mess'}>10</div>
+                    </li>
+                </ul>
+            </li>
         </ul>
     );
 }
