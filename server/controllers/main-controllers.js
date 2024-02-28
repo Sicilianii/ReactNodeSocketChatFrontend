02ec837db61e5037b7b149517a29fc82aae1b94c
Vladislav <98421574+Sicilianii@users.noreply.github.com>
2024-02-28 23:59:25 +0300

    1

--- server/controllers/main-controllers.js
+++ server/controllers/main-controllers.js
@@ -8,12 +8,14 @@ const handleError = (res, err) => { res.status(500).json( {error: `${err}`} )}
 
 const getFullInfo = (req, res) => {
     User.findById(req.query.id).then( (user) => {
-        let DATA = user;
-        const recent = RecentChat.find({ '_id': { $in: user.chats.recentChats } })
-        const groupe = GroupChat.find({ '_id': { $in: user.chats.groupChats } })
-        DATA.chats.recentChats = recent;
-        DATA.chats.groupChats = groupe;
-        res.status(200).json(DATA);
+        RecentChat.find({ '_id': { $in: user.chats.recentChats } }).then( (recentChat) => {
+            GroupChat.find({ '_id': { $in: user.chats.groupChats } }).then( (groupChat) => {
+                let DATA = {...user._doc}
+                DATA.chats.recentChats = recentChat;
+                DATA.chats.recentChats = groupChat;
+                res.status(200).json(DATA);
+            }).catch( (err) => handleError(res, err) )
+        } ).catch( (err) => handleError(res, err) )
     }).catch( (err) => handleError(res, err) )
 }
 
