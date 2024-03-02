86bba2b322765111b21a8d58aaf28acfaea3490b
Sicilianii <joker92006@mail.ru>
2024-03-03 00:16:02 +0300

    start1

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -2,9 +2,10 @@ import { BrowserRouter, createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Chats/Home";
 import { Provider } from "react-redux";
 import React from "react";
-import GetStarted from "../pages/GetStarted/getStarted";
+
 import NotFound from "../pages/NotFound/NotFound";
 import {STORE} from "./store/store";
+import GetStarted from "../pages/getStarted/getStarted";
 
 export default function App() {
 
