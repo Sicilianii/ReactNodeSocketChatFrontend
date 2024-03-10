7e3e17c88f5f6c5c1afd9dd6f937bfc71d1b31d5
Sicilianii <joker92006@mail.ru>
2024-03-10 20:36:50 +0300

    slice backend

--- server/routes/groupChats-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getAllGroupChats } = require('../controllers/groupChat-controllers');
-
-const router = express.Router();
-
-router.get('/groups', getAllGroupChats)  // получаем все чаты пользователя
-
-module.exports = router;
