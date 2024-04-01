21151c96f4dfbad817150a55521dab8705d7ac8c
Sicilianii <joker92006@mail.ru>
2024-04-01 18:48:24 +0300

    2

--- src/pages/SingIn/SingIn.jsx
+++ src/pages/SingIn/SingIn.jsx
@@ -1,34 +1,58 @@
 import WhitePurpleButtonShared from "../../shared/ui/WhitePurpleButtonShared/WhitePurpleButtonShared";
 import React, {useState} from "react";
 import md5 from 'md5';
+import { json } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 
 export default function SingIn() {
 
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+    const [email, setEmail] = useState({
+        value: '',
+        valid: true
+    });
+    const [pass, setPass] = useState({
+        value: '',
+        valid: true
+    });
     const [loading, setLoading] = useState(false);
+    const dispatch = useDispatch();
 
 
-    const trySingIn = async (event) => {
-        event.preventDefault;
+    const trySingIn = async () => {
         setLoading(true);
+        console.log(email, pass, 'its data User');
         await fetch('/singIn', {
             method: "POST",
-            body: {
-                email: email,
-                pass: md5(pass)
-            }
+            mode: "cors",
+            cache: "no-cache",
+            credentials: "same-origin",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            redirect: "follow",
+            referrerPolicy: "no-referrer",
+            method: "POST",
+            body: JSON.stringify({
+                "email": email,
+                "pass": md5(pass)
+            })
+        }).then(data => data.json())
+        .then(user => {
+            // dispatch();
+            setLoading(false);
         })
+        .catch(e => { console.log(e); setLoading(false) })
     }
 
-    const changeEmailInput = (email) => {}
-    const changePassInput = (pass) => {}
+    // const changeEmailInput = (email) => {}
+    // const changePassInput = (pass) => {}
 
     return(
         <>
             <form className={'sing'} onSubmit={(e) => {
-                trySingIn(e)
+                e.preventDefault();
+                trySingIn();
             }}>
                 <h1 className={'sing__h1'}>Welcome back!</h1>
                 <span className={'sing__sp'}>Sign In</span>
@@ -37,7 +61,9 @@ export default function SingIn() {
                     type="text"
                     placeholder={'Email'}
                     onChange={(e) => {
-                        changeEmailInput(e.target.value)
+                        setEmail( (pervstate) => {
+                            return {...pervstate, value: e.target.value}
+                        })
                     }}
                 />
                 <input
@@ -45,7 +71,9 @@ export default function SingIn() {
                     type="password"
                     placeholder={'Password'}
                     onChange={(e) => {
-                        changePassInput(e.target.value)
+                        setPass( (pervstate) => {
+                            return {...pervstate, value: md5(e.target.value)}
+                        })
                     }}
                 />
                 <WhitePurpleButtonShared fontSIzeBnt={'18px'} paddingGtn={'8px 40px'}>Log In</WhitePurpleButtonShared>
