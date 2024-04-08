f08fef349ceb0b441c0f28e6703e5d3e3d9d3085
Sicilianii <joker92006@mail.ru>
2024-04-08 17:36:29 +0300

    done

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,15 +1,44 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import {UserReducer} from "../slice/usersSlice";
 import {RecentChatReducer} from "../slice/recentChatSlice";
 import {GroupChatReducer} from "../slice/groupChatSlice";
 import {profileReducer} from "../slice/profileInfoSlice";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({
+    user: UserReducer,
+    group: GroupChatReducer,
+    recent: RecentChatReducer,
+    profile: profileReducer
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const STORE = configureStore({
-    reducer: {
-        user: UserReducer,
-        group: GroupChatReducer,
-        recent: RecentChatReducer,
-        profile: profileReducer
-    }
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
+
+export const persistor = persistStore(STORE);
+
 
